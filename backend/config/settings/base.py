476ffd460b8a13bb0ebf17f8c91eb62817c21e4f
dihuan <449134710@qq.com>
2025-08-26 11:11:43 +0800

    feat(config): 添加多语言支持并调整时区
    - 在 settings 中添加了多语言支持，包括中文和英文
    - 将默认语言设置为中文 (zh-hans)
    - 将时区设置为 Asia/Shanghai
    - 替换了 django.contrib.admin 为 config.apps.DoccanoAdminConfig

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -33,7 +33,7 @@ DEBUG = env.bool("DEBUG", True)
 # Application definition
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
-    "django.contrib.admin",
+    "config.apps.DoccanoAdminConfig",  # 替换 "django.contrib.admin"
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -175,11 +175,17 @@ REST_FRAMEWORK = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "zh-hans"
+TIME_ZONE = "Asia/Shanghai"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# 添加语言支持
+LANGUAGES = [
+    ('zh-hans', 'Chinese'),
+    ('en', 'English'),
+]
 
 # Testing
 TEST_RUNNER = "xmlrunner.extra.djangotestrunner.XMLTestRunner"
